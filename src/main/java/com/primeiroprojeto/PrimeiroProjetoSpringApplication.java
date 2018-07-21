dd0075814175d69b75621f3090518b7debcb1c97
Bruno Landeiro <landeiro.b@gmail.com>
2018-07-21 09:38:17 -0300

    projeto criado

---
+++ src/main/java/com/primeiroprojeto/PrimeiroProjetoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.primeiroprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+	}
+}
