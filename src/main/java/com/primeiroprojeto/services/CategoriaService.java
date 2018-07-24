3bc5cc30ee3f0e523b8899483793e0db29591abb
Bruno Landeiro <landeiro.b@gmail.com>
2018-07-24 09:34:30 -0300

    Validação sintatica com bean validation

--- src/main/java/com/primeiroprojeto/services/CategoriaService.java
+++ src/main/java/com/primeiroprojeto/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.primeiroprojeto.domain.Categoria;
+import com.primeiroprojeto.dto.CategoriaDTO;
 import com.primeiroprojeto.repositories.CategoriaRepository;
 import com.primeiroprojeto.services.exceptions.DataIntegrityException;
 import com.primeiroprojeto.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
