205d010270226afa8993a4186961707804ef35cd
Bruno Landeiro <landeiro.b@gmail.com>
2018-07-24 10:08:26 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/primeiroprojeto/resources/exceptions/ValidationError.java
@@ -0,0 +1,27 @@
+package com.primeiroprojeto.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> erros = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErros() {
+		return erros;
+	}
+
+	public void addErros(String fieldName, String message) {
+		erros.add(new FieldMessage(fieldName, message));
+	}
+	
+	
+	
+	
+	
+}
