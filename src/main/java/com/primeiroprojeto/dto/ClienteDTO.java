95e6f5bdc846086cb5868fd3b36aac45993f12f9
Bruno Landeiro <landeiro.b@gmail.com>
2018-07-25 09:48:20 -0300

    validagem de email no update de cliente

--- src/main/java/com/primeiroprojeto/dto/ClienteDTO.java
+++ src/main/java/com/primeiroprojeto/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.primeiroprojeto.domain.Cliente;
+import com.primeiroprojeto.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
